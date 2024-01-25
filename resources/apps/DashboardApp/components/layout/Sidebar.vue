37ee2b0d8c29f8a0e76f2e99b8718f5831dd8a88
mhQady <m.qad@neopplanet.com>
2024-01-25 19:25:50 +0200

    Dash: Fix sidebar nav behavior

--- resources/apps/DashboardApp/components/layout/Sidebar.vue
+++ resources/apps/DashboardApp/components/layout/Sidebar.vue
@@ -1,23 +1,39 @@
 <script setup>
 import { onMounted } from 'vue';
-import { RouterLink } from 'vue-router';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
 onMounted(() => {
     initNavBar();
 })
 
 function initNavBar() {
     let navLinks = document.querySelectorAll('.nav__item:has(.sidebar__collapse) .nav__link');
+    let innerMenus = document.querySelectorAll('.sidebar__collapse');
 
     navLinks.forEach(link => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            console.log(link);
+            console.log(route.name, route.name?.startsWith('products'));
             let target = link.dataset.target;
 
             if (target) {
                 link.classList.toggle('active');
-                document.querySelector(`#${target}`).classList.toggle('show');
+                navLinks.forEach(el => {
+                    if (el === link) return;
+
+                    link.classList.remove('active');
+                })
+
+
+                let activeInnerMenu = document.querySelector(`#${target}`);
+                activeInnerMenu.classList.toggle('show')
+
+                innerMenus.forEach(el => {
+                    if (el === activeInnerMenu) return;
+
+                    el.classList.remove('show');
+                })
             }
         })
     })
@@ -37,40 +53,43 @@ function initNavBar() {
                     <div class="icon shadow text-center d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-house"></i>
                     </div>
-                    <span>Dashboard</span>
+                    <span>{{ $t('dashboard') }}</span>
                 </a>
             </li> -->
 
             <li class="nav__item">
-                <a href="#products-nav" data-target="products-nav" class="nav__link">
+                <a href="#products-nav" data-target="products-nav" class="nav__link"
+                    :class="{ 'active': route.name?.startsWith('products') }">
                     <div class="icon shadow text-center d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-carrot"></i>
                     </div>
                     <span>{{ $t('products') }}</span>
                 </a>
-                <div class="sidebar__collapse" id="products-nav">
+                <div class="sidebar__collapse" id="products-nav" :class="{ 'show': route.name?.startsWith('products') }">
                     <ul class="sidebar__nav">
                         <li class="nav__item">
                             <RouterLink :to="{ name: 'products' }" class="nav__link">
-                                <span>products</span>
+                                <span>{{ $t('products') }}</span>
                             </RouterLink>
                         </li>
                         <li class="nav__item ">
                             <RouterLink :to="{ name: 'products.create' }" class="nav__link">
-                                <span>add product</span>
+                                <span>{{ $t('create.product') }}</span>
                             </RouterLink>
                         </li>
                     </ul>
                 </div>
             </li>
             <li class="nav__item">
-                <a href="#categories-nav" data-target="categories-nav" class="nav__link">
+                <a href="#categories-nav" data-target="categories-nav" class="nav__link"
+                    :class="{ 'active': route.name?.startsWith('categories') }">
                     <div class="icon shadow text-center d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-list"></i>
                     </div>
                     <span>{{ $t('categories') }}</span>
                 </a>
-                <div class="sidebar__collapse" id="categories-nav">
+                <div class="sidebar__collapse" id="categories-nav"
+                    :class="{ 'show': route.name?.startsWith('categories') }">
                     <ul class="sidebar__nav">
                         <li class="nav__item">
                             <RouterLink :to="{ name: 'categories' }" class="nav__link">
@@ -79,7 +98,7 @@ function initNavBar() {
                         </li>
                         <li class="nav__item ">
                             <RouterLink :to="{ name: 'categories.create' }" class="nav__link">
-                                <span>{{ $t('create.product') }}</span>
+                                <span>{{ $t('create.category') }}</span>
                             </RouterLink>
                         </li>
                     </ul>
